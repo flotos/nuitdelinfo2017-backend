e3047c0f7039172e70cff5ea9461df17de4c049d
flotos <flotos@hotmail.fr>
2017-12-07 21:42:58 +0000

    Firebase support for POST new user

--- server.py
+++ server.py
@@ -1,35 +1,41 @@
 from flask import request, url_for
 from flask_api import FlaskAPI, status, exceptions
+import pyrebase
+
+config = {
+  "apiKey": "AIzaSyBNLdPrHLEnvFJUOTSrAOnvwj9yA6D0WAo",
+  "authDomain": "nuitdelinfo2017-4ba50.firebaseapp.com",
+  "databaseURL": "https://nuitdelinfo2017-4ba50.firebaseio.com",
+  "storageBucket": "nuitdelinfo2017-4ba50.appspot.com",
+  "serviceAccount": "firebaseKey.json"
+}
+
+firebase = pyrebase.initialize_app(config)
+# Get a reference to the database service
+db = firebase.database()
+# Get a reference to the auth service
+# auth = firebase.auth()
 
 app = FlaskAPI(__name__)
 
 users = {
     0: {
-        "userId": 1,
         "gpsX": 5,
         "gpsY": 10,
         "matchedWith": 0,
     },
     1: {
-        "userId": 2,
         "gpsX": 5,
         "gpsY": 10,
         "matchedWith": 3,
     },
     2: {
-        "userId": 3,
         "gpsX": 5,
         "gpsY": 10,
         "matchedWith": 2,
     },
 }
 
-
-def users_repr(key):
-    return {
-        'url': request.host_url.rstrip('/') + url_for('notes_detail', key=key),
-        'users': users[key]
-    }
 
 @app.route("/findMatch", methods=['GET'])
 def tryFindMatch():
@@ -44,16 +50,17 @@ def notes_list():
         gpsX = str(request.data.get('gpsX', ''))
         gpsY = str(request.data.get('gpsY', ''))
         idx = max(users.keys()) + 1
-        users[idx] = {
-            "userId": idx,
+        newUser = {
             "gpsX": gpsX,
             "gpsY": gpsY,
             "matchedWith": 0,
         },
-        return users_repr(idx), status.HTTP_201_CREATED
+        results = db.child("users/"+request.data.get('userId', '')).set(newUser)
+
+        return results, status.HTTP_201_CREATED
 
     # request.method == 'GET'
-    return [users_repr(idx) for idx in sorted(users.keys())]
+    return db.child("users").get().val()
 
 
 @app.route("/<int:key>/", methods=['GET', 'PUT', 'DELETE'])
@@ -71,9 +78,9 @@ def notes_detail(key):
     #     return '', status.HTTP_204_NO_CONTENT
 
     # request.method == 'GET'
-    if key not in users:
-        raise exceptions.NotFound()
-    return users_repr(key)
+    # if key not in users:
+    #     raise exceptions.NotFound()
+    # return users_repr(key)
 
 
 if __name__ == "__main__":
